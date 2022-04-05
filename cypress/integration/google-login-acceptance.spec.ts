58d82642edfe3ee89ed4f7e774b715adb9264b79
Evan Coulson <evan.m.coulson@gmail.com>
2022-04-05 16:11:24 -0700

    Created acceptance tests

---
+++ cypress/integration/google-login-acceptance.spec.ts
@@ -0,0 +1,5 @@
+describe('Google Login Acceptance Test Suite', () => {
+    test('When user clicks login button it should get token, display success, and redirect', () => {
+        cy.visit('http://localhost:3000');
+    });
+});
