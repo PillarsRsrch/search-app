304749d15f00c099092305eddfb0713c30ad7ced
Evan Coulson <evan.m.coulson@gmail.com>
2022-04-09 16:28:31 -0700

    Refactored authentication page

--- src/views/components/authentication/AuthenticationButton.tsx
+++ src/views/components/authentication/AuthenticationButton.tsx
@@ -6,7 +6,6 @@ export const AuthenticationButton = ({
     onClick,
 }: IAuthenticationButtonProps) => (
     <div className="authentication-button" onClick={(e) => onClick(e)}>
-        <p>Login with google to create a new project</p>
         {children}
     </div>
 );
--- src/views/pages/authentication/AuthenticationPage.tsx
+++ src/views/pages/authentication/AuthenticationPage.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Text } from '../../bases/text/Text';
 import { AuthenticationButton } from '../../components/authentication/AuthenticationButton';
 import { IAuthenticationPageProps } from './IAuthenticationPageProps';
 
 export const AuthenticationPage = ({ children }: IAuthenticationPageProps) => (
-    <AuthenticationButton onClick={() => null}>{children}</AuthenticationButton>
+    <>
+        <Text>Login with google to create a new project</Text>
+        <AuthenticationButton onClick={() => null}>
+            {children}
+        </AuthenticationButton>
+    </>
 );
