56d277809b4ccf05e9b67e63755bf9621effacf0
Evan Coulson <evan.m.coulson@gmail.com>
2022-04-11 14:32:50 -0700

    Added layout component

---
+++ src/views/bases/layout/Layout.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+import { ILayoutProps } from './ILayoutProps';
+
+export const Layout = ({ children }: ILayoutProps) => <Box>{children}</Box>;
