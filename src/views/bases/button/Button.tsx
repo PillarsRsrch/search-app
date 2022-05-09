9b1c6cd594b6cecf44415794b79d40439285e41d
Evan Coulson <evan.m.coulson@gmail.com>
2022-05-09 14:30:42 -0700

    fixed button styles

--- src/views/bases/button/Button.tsx
+++ src/views/bases/button/Button.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { IButtonProps } from './IButtonProps';
 import { Button as ChakraButton } from '@chakra-ui/react';
+import { StyledButtonContainer } from './StyledButtonContainer';
 
 export const Button = ({
     children,
     onClick,
     disabled,
 }: IButtonProps): JSX.Element => (
-    <ChakraButton
-        className="button-component"
-        onClick={onClick}
-        disabled={disabled}
-    >
-        {children}
-    </ChakraButton>
+    <StyledButtonContainer>
+        <ChakraButton
+            className="button-component"
+            onClick={onClick}
+            disabled={disabled}
+        >
+            {children}
+        </ChakraButton>
+    </StyledButtonContainer>
 );
